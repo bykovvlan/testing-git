e445f436a034487277c2afe06b78028d39b82a11
bykov_vlad <bykov.vl.an@gmail.com>
2022-05-03 04:50:10 +0300

    хуня якась

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello World!!!!'
 
 
 if __name__ == '__main__':
     app.run()
-#
